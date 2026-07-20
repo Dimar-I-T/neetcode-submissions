c55b34ddd9d7c834b13953034076231eacd5e4ae
Dimar Ilham Tamara <dimartamara1@gmail.com>
2026-07-20 17:33:15 +0700

    Add: first-missing-positive - submission-2

---
+++ Data Structures & Algorithms/first-missing-positive/submission-2.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        int add = 1000000;
+        for (int x = 0; x < n; x++) {
+            int curr = nums[x];
+            if (curr < 1 || (curr > n && curr <= add)) {
+                nums[x] = 0;
+                continue;
+            }
+
+            int idx = (curr > add) ? curr - add - 1: curr - 1;
+            if (idx >= n || idx < 0) {
+                continue;
+            }
+
+            nums[idx] += add;
+        }
+
+        int res = 1;
+        for (int x = 0; x < n; x++) {
+            if (nums[x] < add) {
+                break;
+            }
+
+            res++;
+        }
+
+        return res;
+    }
+};
